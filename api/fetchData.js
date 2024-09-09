7552319cb8b76045c7c2c6d5a23d352981798c32
unknown <maria.v@mynomademail.com>
2024-09-09 22:46:06 +0200

    function restore

--- api/fetchData.js
+++ api/fetchData.js
@@ -4,26 +4,33 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email, message } = req.body
 
+    if (!name || !email || !message) {
+      return res
+        .status(400)
+        .json({ success: false, error: 'Todos los campos son requeridos.' })
+    }
+
     try {
       const result = await emailjs.send(
-        process.env.SERVICE_ID,
-        process.env.TEMPLATE_ID,
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
         {
           from_name: name,
           from_email: email,
           message: message,
         },
-        process.env.USER_ID,
+        process.env.EMAILJS_USER_ID,
       )
 
-      res
+      return res
         .status(200)
         .json({ success: true, message: 'Email sent successfully!' })
     } catch (error) {
-      res.status(500).json({ success: false, error: error.message })
+      console.error('Error sending email:', error.message)
+      return res.status(500).json({ success: false, error: error.message })
     }
   } else {
     res.setHeader('Allow', ['POST'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
+    return res.status(405).end(`Method ${req.method} Not Allowed`)
   }
 }
